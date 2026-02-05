f734bb70c9f70f62df4fc0819637e46104eaf91a
cahaya-malam <winz44winz@gmail.com>
2026-02-06 00:48:32 +0800

    Update LayarKacaPlugin.kt

--- LayarKaca/src/main/kotlin/com/layarkaca/LayarKacaPlugin.kt
+++ LayarKaca/src/main/kotlin/com/layarkaca/LayarKacaPlugin.kt
@@ -1,17 +1,26 @@
 package com.layarkaca
 
 import android.content.Context
+import com.lagradost.cloudstream3.extractors.Voe
+import com.lagradost.cloudstream3.extractors.StreamTape
+import com.lagradost.cloudstream3.extractors.MixDrop
+import com.lagradost.cloudstream3.extractors.Mp4Upload
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 
 @CloudstreamPlugin
 class LayarKacaPlugin : Plugin() {
     override fun load(context: Context) {
-        // All providers should be added in this manner. Please don't edit the providers list
-        // directly.
         registerMainAPI(LayarKaca())
+        
+        // Hanya gunakan extractor yang umum dan pasti ada
+        registerExtractorAPI(Voe())
+        registerExtractorAPI(StreamTape())
+        registerExtractorAPI(MixDrop())
+        registerExtractorAPI(Mp4Upload())
         registerExtractorAPI(Emturbovid())
 //        registerExtractorAPI(Furher())
         registerExtractorAPI(Hownetwork())
+
     }
 }
